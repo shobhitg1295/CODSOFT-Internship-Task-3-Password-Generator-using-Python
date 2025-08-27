06cd77e7cf29fe3168f2352cc4c5e1068d42e8c5
shobhitg1295 <shobhitg1295@gmail.com>
2025-08-27 22:15:05 +0530

    Update Password-Generator.py

--- Task-3/Password-Generator.py
+++ Task-3/Password-Generator.py
@@ -1,15 +1,18 @@
 import random
 
-length = int(input("Enter the desired password length: "
+# ask user for length
+length = int(input("Enter the desired password length: "))
+
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "!@#$%^&*()_-+=<>?/"
 
-characters = letters + numbers + symbols
 
+characters = letters + numbers + symbols
 password = ""
 
 for i in range(length):
     password = password + random.choice(characters)
 
 print("Generated Password:", password)
+
